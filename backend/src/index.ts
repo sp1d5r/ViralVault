4e84997b8d7fd4bf8f587a21663db7720856708f
Elijah <aa1719@imperial.ac.uk>
2025-06-29 16:31:24 +0100

    Backend: updated the url.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ const port = process.env.PORT || 3001;
 
 // CORS configuration
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Your frontend URL
+  origin: process.env.FRONTEND_URL || 'https://viral-vault.theahmadcompany.com', // Your frontend URL
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'stripe-signature'],
